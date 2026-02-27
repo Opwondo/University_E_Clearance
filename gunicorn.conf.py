08683de1ba3ed990468ad753d1938e12200c17ae
Opwondo <opwondow@gmail.com>
2026-02-27 23:38:37 +0300

    Fix: Bind to 0.0.0.0: for Render deployment

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,13 @@
 import multiprocessing
+import os
 
-bind = "0.0.0.0:8000"
+bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
 worker_class = "sync"
 timeout = 120
 max_requests = 1000
 max_requests_jitter = 50
+loglevel = "info"
+accesslog = "-"
+errorlog = "-"  
